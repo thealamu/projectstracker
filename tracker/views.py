b58c1ad4a24276fce217418c73241df62b999db3
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-03 19:06:35 +0100

    Retrieve synopsis data from the db

--- tracker/views.py
+++ tracker/views.py
@@ -3,7 +3,12 @@ from .models import Project
 
 
 def index(request):
+    projects = Project.objects.all()
     context = {
-        'projects': Project.objects.all()
+        'projects': projects,
+        'n_in_progress': projects.filter(state=2).count(),
+        'n_paused': projects.filter(state=1).count(),
+        'n_shipped': projects.filter(state=4).count(),
+        'n_total': projects.count(),
     }
     return render(request, 'tracker/index.html', context)
