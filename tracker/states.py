84efc2bef3bd4c2642bba052e2ecb8a4a4ca3fa6
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-03 19:40:13 +0100

    Create function that returns id for a project state

--- tracker/states.py
+++ tracker/states.py
@@ -1,6 +1,13 @@
 states = ["Buried", "Paused", "In progress", "Shipping soon", "Shipped"]
 
 
+def id_for_string(state_str):
+    try:
+        return states.index(state_str.capitalize())
+    except ValueError:
+        return 0
+
+
 class State:
     def __init__(self, state_id):
         if state_id < 0 or state_id > 4:
--- tracker/states_test.py
+++ tracker/states_test.py
@@ -1,5 +1,6 @@
 import unittest
 from states import State
+from states import id_for_string
 
 
 class TestState(unittest.TestCase):
@@ -14,6 +15,11 @@ class TestState(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             State(5)
+
+    def test_id_for_string(self):
+        self.assertEqual(id_for_string("PAUSED"), 1)
+        self.assertEqual(id_for_string("Shipping soon"), 3)
+        self.assertEqual(id_for_string("Doesn't exist"), 0)
 
 
 if __name__ == '__main__':
