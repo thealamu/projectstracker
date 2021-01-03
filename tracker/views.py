39f46fe292db6216906af1f5a0d5785418617c77
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-03 19:54:16 +0100

    Use filtered projects list in templates

--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from .models import Project
+from .states import id_for_string
 
 
 def index(request):
     projects = Project.objects.all()
+
+    filter_state = request.GET.get("filter", "all")
+    filter_state_id = id_for_string(filter_state)
+
     context = {
-        'projects': projects,
+        'projects': projects.filter(state=filter_state_id),
         'n_in_progress': projects.filter(state=2).count(),
         'n_paused': projects.filter(state=1).count(),
         'n_shipped': projects.filter(state=4).count(),
