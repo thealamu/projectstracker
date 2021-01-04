e07e7d84f3ca2e1adeace24e1296d33db86e1b70
Faithfulness Alamu <vaguemail369@gmail.com>
2021-01-04 11:16:20 +0100

    Return all projects on bad filter

--- tracker/views.py
+++ tracker/views.py
@@ -9,11 +9,16 @@ def index(request):
     filter_state = request.GET.get("filter", "all")
     filter_state_id = id_for_string(filter_state)
 
+    filtered_projects = projects
+    if filter_state_id > -1:
+        filtered_projects = projects.filter(state=filter_state_id)
+
     context = {
-        'projects': projects.filter(state=filter_state_id),
+        'projects': filtered_projects,
         'n_in_progress': projects.filter(state=2).count(),
         'n_paused': projects.filter(state=1).count(),
         'n_shipped': projects.filter(state=4).count(),
         'n_total': projects.count(),
     }
+
     return render(request, 'tracker/index.html', context)
